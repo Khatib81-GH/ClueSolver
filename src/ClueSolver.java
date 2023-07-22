0c3c4679cd53c38da4dcf7b2430a3d382e8c7657
Khatib81-GH <khatibm2017@gmail.com>
2023-07-22 02:06:18 -0400

    Added gitignore for output

--- src/ClueSolver.java
+++ src/ClueSolver.java
@@ -1,16 +1,32 @@
 public class ClueSolver {
-    public ClueSolver(ClueLevel difficulty) {
+    private ClueLevel difficulty;
+    private boolean timerMode;
+    private boolean clueLimitMode;
+    private int timeInMinutes;
+    private int clueLimit;
+
+    public ClueSolver(ClueLevel difficulty, boolean timerMode, boolean clueLimitMode, int timeInMinutes, int clueLimit) {
+        this.difficulty = difficulty;
+        this.timerMode = timerMode;
+        this.clueLimitMode = clueLimitMode;
+        this.timeInMinutes = timeInMinutes;
+        this.clueLimit = clueLimit;
+
         System.out.println("Solving " + difficulty + " clues.");
 
+        // Use the additional information here to customize the clue-solving logic as needed
+        if (timerMode) {
+            System.out.println("Timer mode is ON with a time limit of " + timeInMinutes + " minutes.");
+        } else {
+            System.out.println("Timer mode is OFF.");
+        }
+
+        if (clueLimitMode) {
+            System.out.println("Clue limit to solve: " + clueLimit);
+        } else {
+            System.out.println("Clue limit mode is OFF.");
+        }
     }
 
-
-
-
-
+    // ... (rest of the code remains unchanged)
 }
-
-
-
-
-
